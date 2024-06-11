c95ddab14fdd91ba498426847b4ed31f7dabe54f
fakerepic <yanglang21@mails.tsinghua.edu.cn>
2024-06-11 12:18:22 +0800

    better chat UI

--- components/chatMessage.tsx
+++ components/chatMessage.tsx
@@ -1,39 +1,36 @@
-import { View, SizableText, Card, Separator, Paragraph } from "tamagui";
+import { View, SizableText, Card, Separator, Paragraph, XStack } from "tamagui";
 import { TouchableOpacity } from "react-native";
 import React from "react";
 import { type TChatMessage, type SourceModel } from "../zustand/chat";
-import Animated, { FadeIn } from "react-native-reanimated";
 import { Link } from "expo-router";
 
-export const ChatMessage = ({
-  item,
-}: {
-  item: TChatMessage;
-  index: number;
-}) => {
+export const ChatMessage = ({ item }: { item: TChatMessage }) => {
+  const isUser = item.role === "user";
   return (
-    <View
-      mb={item.role === "user" ? "$2" : "$4"}
+    <XStack
+      mb={isUser ? "$2" : "$4"}
       mt="$2"
-      ml={item.role === "user" ? "16%" : "$3"}
-      mr={item.role === "user" ? "$3" : "16%"}
+      jc={isUser ? "flex-end" : "flex-start"}
+      ml={isUser ? "$6" : "$3"}
+      mr={isUser ? "$3" : "$6"}
     >
-      <Animated.View entering={FadeIn.delay(30)}>
-        <Card
-          p="$3"
-          bg={item.role === "user" ? "$color5" : "$color2"}
-          bc="$color5"
-          bw={1}
-          borderBottomLeftRadius={item.role === "user" ? "$4" : 0}
-          borderBottomRightRadius={item.role === "user" ? 0 : "$4"}
-        >
-          <Paragraph selectable>{item.content}</Paragraph>
-          {item.sources && item.sources.length > 0 && (
-            <SourceLinks sources={item.sources} />
-          )}
-        </Card>
-      </Animated.View>
-    </View>
+      <Card
+        p="$3"
+        maw={500}
+        animation="bouncy"
+        enterStyle={{ o: 0 }}
+        bg={isUser ? "$color5" : "$color2"}
+        bc="$color5"
+        bw={1}
+        borderBottomLeftRadius={isUser ? "$4" : 0}
+        borderBottomRightRadius={isUser ? 0 : "$4"}
+      >
+        <Paragraph selectable>{item.content}</Paragraph>
+        {item.sources && item.sources.length > 0 && (
+          <SourceLinks sources={item.sources} />
+        )}
+      </Card>
+    </XStack>
   );
 };
 
